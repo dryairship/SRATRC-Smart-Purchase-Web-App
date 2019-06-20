d29db41111163ff663f02f1bb832fb81a146c0b5
jayeshaw <jay.is.united@gmail.com>
2019-06-20 18:49:03 +0530

    :white_check_mark: Create inventory model and database functions

---
+++ db/inventoryItem.js
@@ -0,0 +1,105 @@
+const InventoryItem = require('../models/inventoryItem.js')
+
+function insertInventoryItem(productID, departmentID, amount){
+    return new Promise((resolve, reject) => {
+        inventoryItem = new InventoryItem({
+            productID: productID, 
+            departmentID: departmentID,
+            amount: amount
+        });
+        inventoryItem.save(function(err){
+            if(err)
+                reject({
+                    status: 400, 
+                    response: "Inventory Item not created"
+                });
+            else
+                resolve({
+                    status: 201, 
+                    response: "Inventory Item successfully created"
+                });
+        });
+    });
+}
+
+function findInventoryItemById(productID, departmentID){
+    return new Promise((resolve, reject) => {
+        InventoryItem.findOne({
+            productID: productID,
+            departmentID: departmentID
+        },
+        '-__v',
+        function(err, inventoryItem){
+            if(err || !inventoryItem)
+                reject({
+                    status: 404,
+                    response: "Inventory Item not found"
+                });
+            else
+                resolve({
+                    status: 200,
+                    response: inventoryItem
+                });
+        }); 
+    });
+}
+
+function deleteInventoryItem(productID, departmentID){
+    return new Promise((resolve, reject) => {
+        InventoryItem.findOneAndDelete({
+            productID: productID, 
+            departmentID: departmentID
+        }, 
+        function(err, inventoryItem){
+            if(err)
+                reject({
+                    status: 500, 
+                    response: "A server error occurred"
+                });
+            else if(!inventoryItem)
+                reject({
+                    status: 404, 
+                    response: "Inventory Item to be deleted"
+                });
+            else 
+                resolve({
+                    status: 202, 
+                    response: "Inventory Item successfully deleted"
+                });
+        });
+    });
+}
+
+function updateInventoryItem(productID, departmentID, newInventoryItem){
+    return new Promise((resolve, reject) => {
+        InventoryItem.updateOne({
+            productID: productID,
+            departmentID: departmentID
+        },
+        newInventoryItem,
+        function(err, result){
+            if(err)
+                reject({
+                    status: 500, 
+                    response: "A server error occurred"
+                });
+            else if(result.n == 0)
+                reject({
+                    status: 404, 
+                    response: "Inventory Item not found"
+                });
+            else if(result.nModified == 0)
+                resolve({
+                    status: 200, 
+                    response: "Inventory Item was already up-to-date"
+                });
+            else 
+                resolve({
+                    status: 202,
+                    response: "Inventory Item successfully updated"
+                });
+        });
+    });
+}
+
+module.exports = { insertInventoryItem, findInventoryItemById, updateInventoryItem, deleteInventoryItem};
