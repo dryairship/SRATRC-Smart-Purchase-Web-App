6e4932db4f4e99a7a11b4a5a046c11aba9264d29
Avinandan22 <avibose@iitk.ac.in>
2019-06-21 10:28:54 +0530

    Set up controllers and routers for vendor

---
+++ routes/vendor.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+const { handleVendorPost, handleVendorGetById, handleVendorDelete, handleVendorUpdate, handleVendorGetAll } = require('../controllers/vendor.js')
+
+var router = express.Router()
+
+router.post('/',handleVendorPost);
+router.get('/',handleVendorGetAll);
+router.get('/:vendorID',handleVendorGetById);
+router.patch('/:vendorID',handleVendorUpdate);
+router.delete('/:vendorID',handleVendorDelete);
+
+module.exports = router;
