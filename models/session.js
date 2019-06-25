c5c5eed1144057102c2d64f841a89065adb7301e
Priydarshi Singh <dryairship@gmail.com>
2019-06-25 18:16:34 +0530

    :construction: Add Session model and db functions

---
+++ models/session.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const uuidv4 = require('uuid/v4');
+
+var SessionSchema = new mongoose.Schema({
+    _id: {
+        type: String,
+        default: function(){
+            return uuidv4();
+        }
+    },
+    user: String,
+    expireAt: Date
+});
+
+SessionSchema.index({ "expireAt": 1 }, { expireAfterSeconds: 0 });
+
+var Session = mongoose.model('Session', SessionSchema);
+
+module.exports = Session;
